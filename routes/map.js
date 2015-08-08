0b998054190bedf795ef5b8474f774af6882949d
Sam <Samuele@MacBook-Pro-di-Samuele-Ferrari.local>
2015-08-08 09:12:12 +0200

    Clean code

--- routes/map.js
+++ routes/map.js
@@ -1,104 +1,106 @@
 var express = require('express');
 var router = express.Router();
 
-var mongoose = require( 'mongoose' );
-var map = require('../models/map'); 
-var mapData = require('../models/mapData'); 
-var mapCheckpoint = require('../models/mapCheckpoint'); 
+var mongoose = require('mongoose');
+var map = require('../models/map');
+var mapData = require('../models/mapData');
+var mapCheckpoint = require('../models/mapCheckpoint');
 
 /* GET all maps*/
-router.get('/all', function(req, res, next) {
-    map.find({},{ '_id': 0 }, function(err, docs){
+router.get('/all', function (req, res, next) {
+    map.find({}, { '_id': 0 }, function (err, docs) {
         res.send(docs);
     });
   
 });
 
 /* GET map init data */
-router.get('/:mapid/base', function(req, res, next) {
-    map.find({"mapId":req.params.mapid},{ '_id': 0 }, function(err, docs){
+router.get('/:mapid/base', function (req, res, next) {
+    map.find({"mapId": req.params.mapid}, { '_id': 0 }, function (err, docs) {
         res.send(docs);
     });
   
 });
 
 /* GET map GeoJSON decorations data */
-router.get('/:mapid/decorations', function(req, res, next) {
-    mapData.find({"id":req.params.mapid},{ '_id': 0 }, function(err, docs){
+router.get('/:mapid/decorations', function (req, res, next) {
+    mapData.find({"id": req.params.mapid}, { '_id': 0 }, function (err, docs) {
         res.send(docs);
     });
   
 });
 
 /* GET checkpoint data */
-router.get('/:mapid/:routeId/:checkpointId/data', function(req, res, next) {
-    mapCheckpoint.find({"mapId":req.params.mapid, "routeId":req.params.routeId, "checkpointId":req.params.checkpointId},{ '_id': 0 }, function(err, docs){
+router.get('/:mapid/:routeId/:checkpointId/data', function (req, res, next) {
+    mapCheckpoint.find({"mapId": req.params.mapid, "routeId": req.params.routeId, "checkpointId": req.params.checkpointId}, { '_id': 0 }, function (err, docs) {
         res.send(docs);
     });
   
 });
 
 /* GET checkpoint data */
-router.get('/:mapid/checkpoints/data', function(req, res, next) {
-    mapCheckpoint.find({"mapId":req.params.mapid},{ '_id': 0 }, function(err, docs){
+router.get('/:mapid/checkpoints/data', function (req, res, next) {
+    mapCheckpoint.find({"mapId": req.params.mapid}, { '_id': 0 }, function (err, docs) {
         res.send(docs);
     });
   
 });
 
 /*POST create a new map*/
-router.post('/create', function(req, res, next) {
-    var query = {"mapId":req.body.mapId};
-    var update = req.body;
-    var options = {upsert: true, new: true};
+router.post('/create', function (req, res, next) {
+    var query = {"mapId": req.body.mapId},
+        update = req.body,
+        options = {upsert: true, new: true};
     
     map.findOneAndUpdate(
-        query, update, options,
+        query,
+        update,
+        options,
         function (err) {
-        if (err) {
-            console.log(err);
-            res.send( { "code":"KO", "message": err.message });
-        }else{
-            console.log("Map created succesfully");
-            res.send( { "code":"OK"});
-        }
+            if (err) {
+                console.log(err);
+                res.send({"code": "KO", "message": err.message});
+            } else {
+                console.log("Map created succesfully");
+                res.send({"code": "OK"});
+            }
     });
      
 });
 
 /*POST create a new map*/
-router.post('/createData', function(req, res, next) {
+router.post('/createData', function (req, res, next) {
     
-    var query = {"id":req.body.id};
-    var update = req.body;
-    var options = {upsert: true, new: true};
+    var query = {"id": req.body.id},
+        update = req.body,
+        options = {upsert: true, new: true};
     
-    mapData.findOneAndUpdate(query, update, options,function (err) {
+    mapData.findOneAndUpdate(query, update, options, function (err) {
         if (err) {
             console.log(err);
-            res.send( { "code":"KO", "message": err.message });
-        }else{
+            res.send({"code": "KO", "message": err.message});
+        } else {
             console.log("MapData created succesfully");
-            res.send( { "code":"OK"});
+            res.send({ "code": "OK"});
         }
     });
      
 });
 
 /*POST create a new map*/
-router.post('/createCheckpoint', function(req, res, next) {
+router.post('/createCheckpoint', function (req, res, next) {
     
-    var query = {"mapId":req.body.mapId, "routeId" : req.body.routeId, "checkpointId": req.body.checkpointId };
-    var update = req.body;
-    var options = {upsert: true, new: true};
+    var query = {"mapId": req.body.mapId, "routeId" : req.body.routeId, "checkpointId": req.body.checkpointId },
+        update = req.body,
+        options = {upsert: true, new: true};
 
-    mapCheckpoint.findOneAndUpdate(query, update, options,function (err) {
+    mapCheckpoint.findOneAndUpdate(query, update, options, function (err) {
         if (err) {
             console.log(err);
-            res.send( { "code":"KO", "message": err.message });
-        }else{
+            res.send({"code": "KO", "message": err.message});
+        } else {
             console.log("Checkpoint created succesfully");
-            res.send( { "code":"OK"});
+            res.send({"code": "OK"});
         }
     });
      
--- routes/users.js
+++ routes/users.js
@@ -1,66 +1,61 @@
 var express = require('express');
-var user = require('../models/users'); 
+var user = require('../models/users');
 var passport = require('passport');
 
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('partials/index');
 });
 
 /*POST create a new user*/
-router.post('/create', function(req, res, next) {
+router.post('/create', function (req, res, next) {
     var newUser = new user(req.body);
     newUser.save(function (err) {
         if (err) {
             console.log(err);
-            res.send( { "code":"KO", "message": err.message });
-        }else{
+            res.send({ "code": "KO", "message": err.message});
+        } else {
             console.log("User created succesfully");
-            res.send( { "code":"OK"});
+            res.send({"code": "OK"});
         }
     });
      
 });
 
 /*POST update a user data*/
-router.post('/update', function(req, res, next) {
+router.post('/update', function (req, res, next) {
     
-    var query = {"email":req.body.email};
-    var update = req.body;
-    var options = {upsert: true, new: true};
+    var query = {"email": req.body.email},
+        update = req.body,
+        options = {upsert: true, new: true};
     
     user.findOneAndUpdate(
-        query, update, options,function (err) {
-        if (err) {
-            console.log(err);
-            res.send( { "code":"KO", "message": err.message });
-        }else{
-            console.log("User created succesfully");
-            res.send( { "code":"OK"});
-        }
+        query, update, options, function (err) {
+            if (err) {
+                console.log(err);
+                res.send({"code": "KO", "message": err.message});
+            } else {
+                console.log("User created succesfully");
+                res.send({"code": "OK"});
+            }
     });
      
 });
 
 /*GET create a new user*/
-router.get('/login', 
-          // passport.authenticate('local', { 
-            //                       failureRedirect: '/users/',
-              //                     failureFlash: false }),
-           
-           function(req, res, next) {
-    var userName = req.query.userName;
-    var pass = req.query.pass;
+router.get('/login', function (req, res, next) {
+    var userName = req.query.userName,
+        pass = req.query.pass;
     
-    user.find({"email":userName, "password":pass},{ '_id': 0 }, function(err, docs){
-        if(docs.length==1){
-            res.send( { "code":"OK", userData : docs});
-        }else if(docs.length==0){
-            res.send( { "code":"KO", "message": "No user found" });
-        }else{
-            res.send( { "code":"KO", "message": "More users with the same email" });
+    user.find({"email": userName, "password": pass}, { '_id': 0 }, function (err, docs) {
+        if (docs.length == 1) {
+            res.send({"code":"OK", userData : docs});
+        } else if(docs.length == 0) {
+            res.send({"code": "KO", "message": "No user found"});
+        } else {
+            res.send({"code": "KO", "message": "More users with the same email"});
         }
     });
     
@@ -69,7 +64,7 @@ router.get('/login',
 
 /*GET retrieve all users*/
 router.get('/all', function(req, res, next) {
-     user.find({},{ '_id': 0 }, function(err, docs){
+     user.find({}, { '_id': 0 }, function (err, docs) {
         res.send(docs);
     });
      
