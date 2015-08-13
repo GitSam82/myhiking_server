545ecb9da9314b0ffbf9034cdfe0061668442d57
Sam <Samuele@MacBook-Pro-di-Samuele-Ferrari.local>
2015-08-14 00:24:34 +0200

    Fix

--- models/mapData.js
+++ models/mapData.js
@@ -1,3 +1,3 @@
 var mongoose = require('mongoose');
-var MapDataSchema = new mongoose.Schema({id: String}, { strict: false });
+var MapDataSchema = new mongoose.Schema({id: String,__v: {type: Number, select: false}}, { strict: false });
 module.exports = mongoose.model('MapData', MapDataSchema);
